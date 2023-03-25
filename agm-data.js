134fcf9ccc31a154f47d4156f5469a9e356e447d
Will Goodwin <willgoodwin37@gmail.com>
2023-03-24 20:35:51 -0400

    First time for everything

---
+++ agm-data.js
@@ -0,0 +1,22 @@
+const agmData = [
+    {
+        id: '1',
+        logo: 'https://upload.wikimedia.org/wikipedia/commons/c/c7/Ford-Motor-Company-Logo.png',
+        votedItems: [
+            'Election of directors',
+            'Ratification of auditor',
+            'Executive compensation'
+        ],
+        proxyDocument: 'https://s201.q4cdn.com/693218008/files/doc_financials/2021/ar/FINAL-2022-Ford-Motor-Company-Proxy-Statement_ADA.pdf'
+    },
+    {
+        id: '2',
+        logo: 'https://upload.wikimedia.org/wikipedia/commons/e/e8/Tesla_logo.png',
+        votedItems: [
+            'Election of directors',
+            'Approval of stock option plan',
+            'Executive compensation'
+        ],
+        proxyDocument: 'https://ir.tesla.com/_flysystem/s3/sec/000156459022027167/tsla-defa14a_20220725-gen.pdf'
+    }
+];
